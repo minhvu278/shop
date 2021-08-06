893694c86f84923d7f184472b98c7723a7d7d064
Do Minh Vu <dominhvu278@gmail.com>
2021-08-06 11:23:51 +0700

    lesson12

---
+++ app/Http/Services/Menu/MenuService.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace App\Http\Services\Menu;
+
+
+use App\Models\Menu;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Str;
+
+class MenuService
+{
+    public function create($request){
+        try {
+            Menu::create([
+                'name' => (string) $request->input('name'),
+                'parent_id' => (int) $request->input('parent_id'),
+                'description' => (string) $request->input('description'),
+                'content' => (string) $request->input('content'),
+                'active' => (string) $request->input('active'),
+                'slug' => Str::slug($request->input('name'), '-')
+            ]);
+
+            Session::flash('success', 'Tạo danh mục thành công');
+        }catch (\Exception $err){
+            Session::flash('error', $err->getMessage());
+            return false;
+        }
+        return true;
+    }
+}
---
+++ resources/views/admin/menu/add.blade.php
@@ -0,0 +1,60 @@
+@extends('admin.main')
+
+@section('head')
+    <script src="/ckeditor/ckeditor.js"></script>
+@endsection
+@section('content')
+    <form action="" method="post">
+        @csrf
+        <div class="card-body">
+
+            <div class="form-group">
+                <label>Tên danh mục</label>
+                <input type="text" name="name" class="form-control" id="menu" placeholder="Enter name">
+            </div>
+
+            <div class="form-group">
+                <label>Danh mục</label>
+                <select class="form-control" name="parent_id" id="">
+                    <option value="0">Danh mục cha</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label>Mô tả</label>
+                <textarea name="description" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Mô tả chi tiết</label>
+                <textarea name="content" id="content" class="form-control"></textarea>
+            </div>
+
+            <div class="form-group">
+                <label>Kích hoạt</label>
+                        <div class="form-check">
+                            <input class="form-check-input" value="1" type="radio" id="active" name="active" checked="">
+                            <label class="form-check-label">Có</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" value="0" type="radio" name="active" id="no_active">
+                            <label class="form-check-label">Không</label>
+                        </div>
+            </div>
+
+        </div>
+
+        <div class="card-footer">
+            <button type="submit" class="btn btn-primary">Thêm danh mục</button>
+
+
+
+        </div>
+    </form>
+@endsection
+
+@section('footer')
+    <script>
+        CKEDITOR.replace('content')
+    </script>
+@endsection
